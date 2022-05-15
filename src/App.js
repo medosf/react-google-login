b2ece3e4d64225c667d150a4026e601577c8e66d
medosf <43324373+medosf@users.noreply.github.com>
2022-05-14 22:02:23 -0700

    update login logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
-import GoogleLogin from 'react-google-login';
-import { useState } from 'react';
+import "./App.css";
+import GoogleLogin from "react-google-login";
+import { useState } from "react";
 
 function App() {
   const [loginData, setLoginData] = useState(
-    localStorage.getItem('loginData')
-      ? JSON.parse(localStorage.getItem('loginData'))
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
       : null
   );
 
@@ -14,42 +14,45 @@ function App() {
   };
 
   const handleLogin = async (googleData) => {
-    const res = await fetch('/api/google-login', {
-      method: 'POST',
-      body: JSON.stringify({
-        token: googleData.tokenId,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    const data = await res.json();
-    setLoginData(data);
-    localStorage.setItem('loginData', JSON.stringify(data));
+    try {
+      const res = await fetch("/api/google-login", {
+        method: "POST",
+        body: JSON.stringify({
+          token: googleData.tokenId,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setLoginData(data);
+      localStorage.setItem("loginData", JSON.stringify(data));
+    } catch (err) {
+      console.log("err", err);
+    }
   };
   const handleLogout = () => {
-    localStorage.removeItem('loginData');
+    localStorage.removeItem("loginData");
     setLoginData(null);
   };
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Google Login App</h1>
+        <h1>React Login App with Google</h1>
         <div>
           {loginData ? (
             <div>
-              <h3>You logged in as {loginData.email}</h3>
+              <img src={loginData?.picture} alt="avatar" />
+              <h3>Hello, {loginData?.name}</h3>
               <button onClick={handleLogout}>Logout</button>
             </div>
           ) : (
             <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              clientId={process.env.REACT_APP_GOOGLE}
               buttonText="Log in with Google"
               onSuccess={handleLogin}
               onFailure={handleFailure}
-              cookiePolicy={'single_host_origin'}
+              cookiePolicy={"single_host_origin"}
             ></GoogleLogin>
           )}
         </div>
